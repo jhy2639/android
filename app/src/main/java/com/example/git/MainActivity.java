00eb08ff56ba6f18ab398645acbf300cddf9087e
smhrd <jhy2639@naver.com>
2021-05-13 12:12:42 +0900

    test22

--- app/src/main/java/com/example/git/MainActivity.java
+++ app/src/main/java/com/example/git/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         int a = 1;
-        String b = "a";
+        String b = "abb";
     }
 }
